fbd2d757524afd0d3693f280307428c2512dae8f
waqarahmed034920@gmail.com <waqarahmed034920@gmail.com>
2020-09-02 22:51:19 +0500

    we made a person api today.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 
 function readFile(filename, res) {
     fs.readFile(filename, function (error, data) {
         if (error) {
             throw error;
-        } else if (filename) {
-             {
-                 res.writeHead(400,{'Content-Type': 'text/html'});
-             }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+        } else {
+
+            if(filename === 'not-found.html') {
+                res.writeHead(400, { 'Content-Type': 'text/html' });
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+            }
+
             res.write(data);
             res.end();
         } 
@@ -17,13 +22,37 @@ function readFile(filename, res) {
 }
 
 var myServer = http.createServer(function (req, res) {
+    var pagename = url.parse(req.url, true).pathname;
+    // path name aap ko page name bata ta ha.
+    // /about, /contactus, /person/insert
 
-    if (req.url === '/') { //localhost:3000
+    if (pagename === '/') { //localhost:3000
         readFile('home.html', res);
-    } else if (req.url === '/contactus') { // locahost:3000/about
+    } else if (pagename === '/contactus') { // locahost:3000/about
         readFile('contactus.html', res);
-    } else if (req.url === '/about') { // locahost:3000/about
+    } else if (pagename === '/about') { // locahost:3000/about
         readFile('about.html', res);
+    } else if (pagename === '/person/insert') { // locahost:3000/about
+        var queryStringData = url.parse(req.url, true).query;
+        console.log('query string data:', queryStringData);
+
+        // req.method === 'PUT'
+        var bodyData = '';
+        req.on('data', function(chunk) {
+            bodyData = bodyData + chunk.toString();
+        });
+
+        req.on('end', function() {
+            console.log('json data from body', bodyData);
+            // now you can insert this data in database.
+            // insert into person(first...) value(...);
+
+            res.writeHead(200, { 'content-type': 'application/json'});
+            res.write(bodyData);
+            res.end();
+        });
+
+
     } else { 
         readFile('not-found.html', res);
     }
@@ -31,3 +60,4 @@ var myServer = http.createServer(function (req, res) {
 myServer.listen(3000);
 
 console.log('my server is listening at port 3000');
+console.log('please access the server from http://localhost:3000/')
